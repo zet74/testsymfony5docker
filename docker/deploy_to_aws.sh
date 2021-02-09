96740a404b72f0d3fa76991af4828dd536090c64
Zet74 <etienne.buffet@gmail.com>
2021-02-09 17:41:05 +0100

    deploy_to_aws.sh

--- docker/deploy_to_aws.sh
+++ docker/deploy_to_aws.sh
@@ -2,10 +2,12 @@
 
 echo 'Starting to Deploy...'
 ssh ubuntu@ec2-15-236-211-78.eu-west-3.compute.amazonaws.com " sudo docker image prune -f
-        cd apps/testsymfony5docker/
+        cd apps/testsymfony5docker/docker/
         sudo docker-compose down
+        cd ..
         git fetch origin
         git reset --hard origin/master  &&  echo 'You are doing well'
+        cd docker/
         sudo docker-compose build && sudo docker-compose up -d
         "
 echo 'Deployment completed successfully'
